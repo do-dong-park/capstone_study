bc91eb2130723cdbc882753cb1dbbb8f70bc0059
bagdong-gyu <rnlgksclsrn9868@gmail.com>
2023-03-23 23:13:49 +0900

    ADD Scikit-learn template

---
+++ models/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from wrappers import *
+from sklearn.svm import SVC
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import Ridge
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.dummy import DummyRegressor
+
+methods_dict = {
+    'ridge': Ridge,
+    'pf': PolynomialFeatures,
+    'scaler': StandardScaler,
+    'PLS':PLSRegressionWrapper,
+    'savgol':SavgolWrapper,
+    'SVC':SVC,
+    'PCA':PCA
+}
